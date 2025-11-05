4d58352eeff95e9b2e489d118d07d3058c9f6c03
ogaiku <genspark_dev@genspark.ai>
2025-11-05 07:29:09 +0000

    config: Update URLs for deployed GAS Web App and Google Forms

--- gas-lightweight-version/website/config.js
+++ gas-lightweight-version/website/config.js
@@ -7,14 +7,13 @@
 const APP_CONFIG = {
   // GAS Web App URL（デプロイ後に取得）
   // 例: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec'
-  GAS_API_URL: 'YOUR_GAS_WEB_APP_URL_HERE',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwP28tt5bsSvHJaciIcOJccFyqsG9TXQgLv6Pai_-1YW7DHoDUgdSiaYPfgsgA6kebuwQ/exec',
   
   // Googleフォーム URL（対戦記録入力用）
-  // 例: 'https://docs.google.com/forms/d/e/YOUR_FORM_ID/viewform'
-  GAME_FORM_URL: 'YOUR_GAME_FORM_URL_HERE',
+  GAME_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdeDHH9OyDtz7lxwRl5cW1lVrMKcDSwunFVJ-8vQ1dSbDNhHQ/viewform',
   
   // Googleフォーム URL（プレイヤー管理用）
-  PLAYER_FORM_URL: 'YOUR_PLAYER_FORM_URL_HERE',
+  PLAYER_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSd7Hnq58VKftUAMo2s5OwazLF2QbrA2QbxJyX6nLcH2LoKWMQ/viewform',
   
   // デフォルト設定
   DEFAULT_RECENT_GAMES_LIMIT: 20,
